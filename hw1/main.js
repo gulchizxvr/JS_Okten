c7605c615e57064120cecb2d8c9ae71cf9f76811
gulchizxvr <kyrylo.hulchenko@gmail.com>
2022-07-22 11:59:09 +0300

    Initial commit

---
+++ hw1/main.js
@@ -0,0 +1,87 @@
+let word1 = 'hello';
+console.log(word1);
+alert(word1);
+document.write(word1 + ' ');
+
+let word2 = 'owu';
+console.log(word2);
+alert(word2);
+document.write(word2 + ' ');
+
+
+let word3 = 'com';
+console.log(word3);
+alert(word3);
+document.write(word3 + ' ');
+
+
+let word4 = 'ua';
+console.log(word4);
+alert(word4);
+document.write(word4 + ' ');
+
+let num1 = 1;
+console.log(num1);
+alert(num1);
+document.write(num1 + ' ');
+
+let num2 = 10;
+console.log(num2);
+alert(num2);
+document.write(num2 + ' ');
+
+let num3 = -999;
+console.log(num3);
+alert(num3);
+document.write(num3 + ' ');
+
+let num4 = 123;
+console.log(num4)
+alert(num4);
+document.write(num4 + ' ');
+
+let num5 = 3.14;
+console.log(num5)
+alert(num5);
+document.write(num5 + ' ');
+
+let num6 = 2.7;
+console.log(num6)
+alert(num6);
+document.write(num6 + ' ');
+
+let bool1 = true;
+console.log(bool1);
+alert(bool1);
+document.write(bool1 + ' ');
+
+let bool2 = false;
+console.log(bool2);
+alert(bool2);
+document.write(bool2 + ' ');
+
+let firstName = 'Кирило';
+let middleName = 'Сергійович';
+let lastName = 'Гульченко';
+
+console.log(firstName + ' ' + middleName + ' ' + lastName)
+
+let a = 100;
+console.log(typeof a);
+
+let b='100';
+console.log(typeof b);
+
+let c = true;
+console.log(typeof c)
+
+let name;
+prompt(name)
+
+let midName;
+prompt(midName);
+
+let year;
+prompt(year);
+
+console.log(name + ' ' + midName + ' ' + year)
