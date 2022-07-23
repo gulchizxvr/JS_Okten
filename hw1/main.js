7778127cea8f0350167a6aff39532b4449cfaf0f
gulchizxvr <kyrylo.hulchenko@gmail.com>
2022-07-23 14:07:30 +0300

    js hw1

---
+++ hw1/main.js
@@ -0,0 +1,93 @@
+let word1 = 'hello';
+console.log(word1);
+alert(word1);
+document.write(word1 + ' ');
+
+let word2 = 'owu';
+console.log(word2);
+alert(word2);
+document.write(word2 + ' ');
+
+let word3 = 'com';
+console.log(word3);
+alert(word3);
+document.write(word3 + ' ');
+
+let word4 = 'ua';
+console.log(word4);
+alert(word4);
+document.write(word4);
+
+let num1 = 1;
+console.log(num1);
+alert(num1);
+document.write(num1 + ' ');
+
+let num2 = 10;
+console.log(num2);
+alert(num2);
+document.write(num2 + ' ');
+
+let num3 = -999;
+console.log(num3);
+alert(num3);
+document.write(num3 + ' ');
+
+let num4 = 123;
+console.log(num4);
+alert(num4);
+document.write(num4 + ' ');
+
+let num5 = 3.14;
+console.log(num5);
+alert(num5);
+document.write(num5 + ' ');
+
+let num6 = 2.7;
+console.log(num6);
+alert(num6);
+document.write(num6 + ' ');
+
+let num7 = 16;
+console.log(num7);
+alert(num7);
+document.write(num7 + ' ');
+
+let bool1 = true;
+console.log(bool1)
+alert(bool1);
+document.write(bool1 + ' ');
+
+let bool2 = false;
+console.log(bool2);
+alert(bool2);
+document.write(bool2 + ' ');
+
+let firstName = 'Кирило';
+let middleName = 'Сергійович';
+let lastName = 'Гульченко';
+
+let person = firstName + ' ' + middleName + ' ' + lastName;
+console.log(person);
+
+let a = 100;
+console.log(typeof a);
+
+let b = '100';
+console.log(typeof b);
+
+let c = true;
+console.log(typeof c);
+
+
+let name = prompt("Твоє ім'я?");
+let midname = prompt("По-батькові?");
+let year = prompt("Скільки тобі років");
+
+let info = name + ' ' + midname + ' ' + year;
+console.log(info);
+
+
+
+
+
